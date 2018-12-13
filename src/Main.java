ed1a892afda2cdff5d10a1413a149f90992560df
alhr31 <alhram31@yandex.ru>
2018-12-14 01:57:32 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("s commit");
+        System.out.println("Second commit");
         System.out.println("t commit");
     }
 }
