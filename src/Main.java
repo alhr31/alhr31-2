1ae38624ae5986ffba7bce93d2bf3c238235068c
alhr31 <alhram31@yandex.ru>
2018-12-14 01:57:10 +0300

    t commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("s commit");
-        System.out.println("Third commit");
+        System.out.println("t commit");
     }
 }
